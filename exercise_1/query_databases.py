c3ea4d78bdd6ba6562023d224396b061920898d7
Mark Raasveldt <mark.raasveldt@gmail.com>
2019-11-04 15:40:03 +0100

    Update slides

--- exercise_1/query_databases.py
+++ exercise_1/query_databases.py
@@ -8,7 +8,7 @@ import pandas
 SCRIPT_PATH =  os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 os.chdir(SCRIPT_PATH)
 
-ncvoters_pandas = pandas.read_csv('ncvoter_sample.tsv', sep='\t', low_memory=False, names=["county_id" , "county_desc" , "voter_reg_num" , "status_cd" , "voter_status_desc" , "reason_cd" , "voter_status_reason_desc" , "absent_ind" , "name_prefx_cd" , "last_name" , "first_name" , "midl_name" , "name_sufx_cd" , "full_name_rep" , "full_name_mail" , "house_num" , "half_code" , "street_dir" , "street_name" , "street_type_cd" , "street_sufx_cd" , "unit_designator" , "unit_num" , "res_city_desc" , "state_cd" , "zip_code" , "res_street_address" , "res_city_state_zip" , "mail_addr1" , "mail_addr2" , "mail_addr3" , "mail_addr4" , "mail_city" , "mail_state" , "mail_zipcode" , "mail_city_state_zip" , "area_cd" , "phone_num" , "full_phone_number" , "drivers_lic" , "race_code" , "race_desc" , "ethnic_code" , "ethnic_desc" , "party_cd" , "party_desc" , "sex_code" , "sex" , "birth_age" , "birth_place" , "registr_dt" , "precinct_abbrv" , "precinct_desc" , "municipality_abbrv" , "municipality_desc" , "ward_abbrv" , "ward_desc" , "cong_dist_abbrv" , "cong_dist_desc" , "super_court_abbrv" , "super_court_desc" , "judic_dist_abbrv" , "judic_dist_desc" , "nc_senate_abbrv" , "nc_senate_desc" , "nc_house_abbrv" , "nc_house_desc" , "county_commiss_abbrv" , "county_commiss_desc" , "township_abbrv" , "township_desc" , "school_dist_abbrv" , "school_dist_desc" , "fire_dist_abbrv" , "fire_dist_desc" , "water_dist_abbrv" , "water_dist_desc" , "sewer_dist_abbrv" , "sewer_dist_desc" , "sanit_dist_abbrv" , "sanit_dist_desc" , "rescue_dist_abbrv" , "rescue_dist_desc" , "munic_dist_abbrv" , "munic_dist_desc" , "dist_1_abbrv" , "dist_1_desc" , "dist_2_abbrv" , "dist_2_desc" , "confidential_ind" , "age" , "ncid" , "vtd_abbrv" , "vtd_desc"])
+ncvoters_pandas = pandas.read_csv('ncvoter_sample.tsv', sep='\t', low_memory=False, names=["county_id" , "county_desc" , "voter_reg_num" , "status_cd" , "voter_status_desc" , "reason_cd" , "voter_status_reason_desc" , "absent_ind" , "name_prefx_cd" , "last_name" , "first_name" , "midl_name" , "name_sufx_cd" , "full_name_rep" , "full_name_mail" , "house_num" , "half_code" , "street_dir" , "street_name" , "street_type_cd" , "street_sufx_cd" , "unit_designator" , "unit_num" , "res_city_desc" , "state_cd" , "zip_code" , "res_street_address" , "res_city_state_zip" , "mail_addr1" , "mail_addr2" , "mail_addr3" , "mail_addr4" , "mail_city" , "mail_state" , "mail_zipcode" , "mail_city_state_zip" , "area_cd" , "phone_num" , "full_phone_number" , "drivers_lic" , "race_code" , "race_desc" , "ethnic_code" , "ethnic_desc" , "party_cd" , "party_desc" , "sex_code" , "sex" , "birth_age" , "birth_place" , "registr_dt" , "precinct_abbrv" , "precinct" , "municipality_abbrv" , "municipality_desc" , "ward_abbrv" , "ward_desc" , "cong_dist_abbrv" , "cong_dist_desc" , "super_court_abbrv" , "super_court_desc" , "judic_dist_abbrv" , "judic_dist_desc" , "nc_senate_abbrv" , "nc_senate_desc" , "nc_house_abbrv" , "nc_house_desc" , "county_commiss_abbrv" , "county_commiss_desc" , "township_abbrv" , "township_desc" , "school_dist_abbrv" , "school_dist_desc" , "fire_dist_abbrv" , "fire_dist_desc" , "water_dist_abbrv" , "water_dist_desc" , "sewer_dist_abbrv" , "sewer_dist_desc" , "sanit_dist_abbrv" , "sanit_dist_desc" , "rescue_dist_abbrv" , "rescue_dist_desc" , "munic_dist_abbrv" , "munic_dist_desc" , "dist_1_abbrv" , "dist_1_desc" , "dist_2_abbrv" , "dist_2_desc" , "confidential_ind" , "age" , "ncid" , "vtd_abbrv" , "vtd_desc"])
 precinctvotes_pandas = pandas.read_csv('precinctvotes.tsv', sep='\t', low_memory=False, names = ["county", "precinct", "total_votes", "romney_percentage"])
 
 
@@ -62,7 +62,7 @@ WHERE ncvoters.status_cd='A' and sex_code = 'M' and race_code = 'W' and birth_ag
 order by  total_white desc Limit 3'''
 
 query_03_duck = '''
-SELECT  county_id, first(romney_percentage),count(*) as total_white
+SELECT county_id, first(romney_percentage),count(*) as total_white
 FROM precinct_votes
 INNER JOIN ncvoters
 ON ncvoters.precinct_desc=precinct_votes.precinct AND ncvoters.county_desc=precinct_votes.county
@@ -103,11 +103,19 @@ def query_02_duckdb():
 	result = duck_cursor.fetchone()
 	print(result)
 
-def query_03_pandas():
+WHERE ncvoters.status_cd=‘A' AND sex_code = 'M'
+  AND race_code = ‘W' AND birth_age > 40
 
+def query_03_pandas():
+voter_status = ncvoters_pandas['status_cd'] == 'A'
+race_code = ncvoters_pandas['race_code'] == 'W'
+birth_age = ncvoters_pandas['birth_age'] > 40
+sex_code = ncvoters_pandas['sex_code'] = 'M'
+ncvoters_pandas[voter_status & race_code & birth_age & sex_code].join(precinctvotes_pandas, on="precinct")[]
+
+	return
 	# result = ncvoters_pandas.groupby(['county_id','race_code']).agg({'county_desc': 'count'})
 	# print(result[result.race_code.index == 'B' > result.race_code.index == 'W'])
-	return;
 
 def query_03_sqlite():
 	sqlite_cursor.execute(query_03)
